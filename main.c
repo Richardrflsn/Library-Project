1f3b637d1bd408bba03e56d0e5f875cbc89932fa
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2023-01-02 16:16:11 +0700

    Welcome message & Menu

--- main.c
+++ main.c
@@ -16,7 +16,6 @@ void printMessageCenter(const char *message){
 }
 
 void headMessage(const char *message){
-    system("cls");
     printf("\t\t\t\t\033[0;35m__________________________________________________");
     printf("\n\n\033[1;37m%65s", "| Library in C |");
     printf("\n\t\t\t\t\033[0;35m__________________________________________________\033[0m");
@@ -26,11 +25,59 @@ void headMessage(const char *message){
 }
 
 void welcomeMessage(){
-    headMessage("");
+    headMessage("Library Management Project");
+    printf("\n\t\t\t\t\033[0;31m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\033[0;33m");
+    printf("\n\t\t\t          .............................................");
+    printf("\n\t\t\t          |                  WELCOME                  |");
+    printf("\n\t\t\t          |                    TO                     |");
+    printf("\n\t\t\t          |                  ORACLE                   |");
+    printf("\n\t\t\t          |                  LIBRARY                  |");
+    printf("\n\t\t\t          .............................................\n");
+    printf("\n\t\t\t\t\033[0;31m^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\033[0m\n");
+    printf("\n\t\t\t\t Enter to continue.....");
+    getchar();
+}
+
+void menu(){
+    headMessage("Main Menu");
+    int option;
+    do
+    {
+        printf("%64s\n", "1. User login");
+        printf("%64s\n", "2. View Books");
+        printf("%66s\n", "3. Search Books");
+        printf("%66s\n", "4. Borrow Books");
+        printf("%58s\n", "5. Exit");
+        printf("\n\n%65s", "Enter option -> ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            /* code */
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    } while (option != 5);
+    
 }
 
 int main()
 {
-    headMessage("Welcome");
+    welcomeMessage();
+    menu();
     return 0;
 }
