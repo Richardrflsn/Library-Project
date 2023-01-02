796636699a7f4c495c54a1fbdeb52557c837c949
Richardrflsn <105922097+Richardrflsn@users.noreply.github.com>
2023-01-02 15:18:05 +0700

    Header library

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printMessageCenter(const char *message){
+    int length = 0;
+
+    length = (68 - strlen(message))/2;
+    printf("\t\t\t");
+    int i;
+    for ( i = 0; i < length; i++)
+    {
+        printf(" ");
+    }
+    printf("%s", message);
+}
+
+void headMessage(const char *message){
+    system("cls");
+    printf("\t\t\t\t\033[0;35m__________________________________________________");
+    printf("\n\n\033[1;37m%65s", "| Library in C |");
+    printf("\n\t\t\t\t\033[0;35m__________________________________________________\033[0m");
+    printf("\n\t\t\t\t\033[0;34m--------------------------------------------------\033[0;36m\n");
+    printMessageCenter(message);
+    printf("\n\t\t\t\t\033[0;34m--------------------------------------------------\033[0m\n");
+}
+
+void welcomeMessage(){
+    headMessage("");
+}
 
 int main()
 {
+    headMessage("Welcome");
     return 0;
 }
